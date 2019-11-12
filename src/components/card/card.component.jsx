392bdde3ec1cfd8f2beee63fca80f29a6b0fe8db
Gabriel CM <gabrielcostamoura1@gmail.com>
2019-11-12 19:49:51 -0300

    added cinemas logos

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -3,6 +3,14 @@ import './card.styles.css'
 
 export const Card = props => (
     <div className="card-container">
+
+        <img 
+            src={props.cinema.images[0].url} 
+            alt="cinema icon" 
+            className="card-image"
+            ></img>
+            
         <h1>{props.cinema.name}</h1>
+
     </div>
 )
