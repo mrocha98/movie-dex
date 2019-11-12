b32c312f74a0f3d32c5dc47c1a93764dcf56246c
Gabriel CM <gabrielcostamoura1@gmail.com>
2019-11-12 19:27:52 -0300

    created firsts components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>hey there folks</h1>
-    </div>
-  );
-}
+// Importando componentes
+import { CardList } from './components/card-list/card-list.component'
 
-export default App;
+export default class App extends React.Component {
+
+  constructor(){
+    // Chama o método construtor na classe Component
+    super()
+
+    this.state = {
+      movies: [],
+      cinemas: []
+    }
+
+  }
+  // Quando este componente for montado, fazer...
+  componentDidMount(){
+    fetch('https://cors-anywhere.herokuapp.com/https://api-content.ingresso.com/v0/theaters/city/46/partnership/moviebot?api_key=moviebot')
+      .then( resp => resp.json() )
+      .then( data => this.setState( { cinemas: data.items } ))
+  }
+
+
+  render(){
+
+    return (
+      <div className="App">
+        <CardList>
+          {
+            this.state.cinemas !== []? this.state.cinemas.map(
+            cinema => <h1 key={cinema.id}>{cinema.name}</h1>
+            ) 
+            
+            : null
+          }
+        </CardList>
+      </div>
+
+    );
+
+  }
+}
