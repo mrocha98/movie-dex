c4f552dbc0224e290477aedc758c2d5fe9ab5d66
root <you@example.com>
2019-11-22 15:41:15 -0300

    Updates

---
+++ precache-manifest.412743b42430d4ef0bfe35449ff7ca2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5f486bb1d6fcae83496797a52551f31",
+    "url": "/movie-dex/index.html"
+  },
+  {
+    "revision": "5a1cc099a5590d7d92d4",
+    "url": "/movie-dex/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "512b75bda79143274313",
+    "url": "/movie-dex/static/css/main.8b8f93c6.chunk.css"
+  },
+  {
+    "revision": "5a1cc099a5590d7d92d4",
+    "url": "/movie-dex/static/js/2.e2d03325.chunk.js"
+  },
+  {
+    "revision": "512b75bda79143274313",
+    "url": "/movie-dex/static/js/main.98338a32.chunk.js"
+  },
+  {
+    "revision": "96d9330b95d2d199bb8c",
+    "url": "/movie-dex/static/js/runtime-main.5cc218df.js"
+  }
+]);
