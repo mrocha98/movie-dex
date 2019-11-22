e25587574bb1593f4722df479a60b26d5bfb3915
root <you@example.com>
2019-11-22 15:15:27 -0300

    added build folder

---
+++ build/precache-manifest.bfe7c4aa85f4b5e3ed99ddd0a7c3a6a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e3ba57a0ab44e870257a881f82b16c0",
+    "url": "/movies-comming-soon/index.html"
+  },
+  {
+    "revision": "efc7f2276dba9889a310",
+    "url": "/movies-comming-soon/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "9f9da22808327680ad79",
+    "url": "/movies-comming-soon/static/css/main.8b8f93c6.chunk.css"
+  },
+  {
+    "revision": "efc7f2276dba9889a310",
+    "url": "/movies-comming-soon/static/js/2.8fff452a.chunk.js"
+  },
+  {
+    "revision": "9f9da22808327680ad79",
+    "url": "/movies-comming-soon/static/js/main.4147cbd2.chunk.js"
+  },
+  {
+    "revision": "1d6059c844246b653d2c",
+    "url": "/movies-comming-soon/static/js/runtime-main.39d2cdbf.js"
+  }
+]);
